2005a3eb58b15066c4daa8f012dcfabf001c58b3
Rémi Thebault <remi.thebault@gmail.com>
2026-03-30 18:34:01 +0200

    [facade] lint:fix

--- facade/env.d.ts
+++ facade/env.d.ts
@@ -2,7 +2,7 @@
 /// <reference types="vite-plugin-vue-layouts-next/client" />
 
 declare module '*.vue' {
-	import type { DefineComponent } from 'vue'
-	const component: DefineComponent<Record<string, unknown>, Record<string, unknown>, unknown>
-	export default component
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<Record<string, unknown>, Record<string, unknown>, unknown>
+  export default component
 }
