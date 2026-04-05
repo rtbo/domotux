6fdc185dc5360623e8bbd76546be01f75c1243af
Rémi Thebault <remi.thebault@gmail.com>
2026-04-05 08:26:47 +0200

    start PowerGauge

---
+++ facade/src/components/PowerGauge.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-stage :config="stageSize">
+    <v-layer>
+      <v-arc
+        :config="{
+          x: stageSize.width / 2,
+          y: stageSize.height / 2,
+          innerRadius: 70,
+          outerRadius: 90,
+          angle: 180,
+          rotation: -90,
+          fill: '#ddd',
+        }"
+      />
+      <v-arc
+        :config="{
+          x: stageSize.width / 2,
+          y: stageSize.height / 2,
+          innerRadius: 70,
+          outerRadius: 90,
+          angle: 90, // Example value, you can bind this to a reactive property for dynamic updates
+          rotation: -90,
+          fill: '#f00',
+        }"
+      />
+    </v-layer>
+  </v-stage>
+</template>
+
+<script setup lang="ts">
+  import { Arc as VArc, Layer as VLayer, Stage as VStage } from 'vue-konva'
+
+  const props = defineProps<{
+    value: number
+  }>()
+
+  const stageSize = {
+    width: 200,
+    height: 200,
+  }
+
+</script>
--- facade/src/pages/Dashboard.vue
+++ facade/src/pages/Dashboard.vue
@@ -2,11 +2,18 @@
   <h1>Domotux Dashboard</h1>
   <p>PApp: {{ dashboardStore.papp }}</p>
   <p>Prix KWh: {{ dashboardStore.kwhPrice }}</p>
+  <v-card
+    outlined
+    :title="$t('message.papp')"
+  >
+    <PowerGauge :value="dashboardStore.papp" />
+  </v-card>
 </template>
 
 <script setup lang="ts">
+  import { onBeforeUnmount } from 'vue'
+  import PowerGauge from '@/components/PowerGauge.vue'
   import { useDashboardStore } from '@/stores/dashboard'
-import { onBeforeUnmount } from 'vue';
 
   const dashboardStore = useDashboardStore()
 
